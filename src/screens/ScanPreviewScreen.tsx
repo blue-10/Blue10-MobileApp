b86407220c221fdce8b50bc783b23218b56db4f1
Rick den Haan <rick@wecreatesolutions.nl>
2023-06-02 11:30:36 +0200

    #44: Uploaden van een scan (WIP)
    Flow gewijzigd bij maken van een scan:
    - Bij openen scan preview scherm wordt nu direct de camera geopend als er nog geen foto's zijn
    - Bij sluiten camera als er geen foto's zijn, navigeer terug naar Dashboard
    - Bij verwijderen laatste foto opent automatisch de camera weer, bij sluiten camera zonder foto's navigeer terug naar Dashboard
    - Empty state achtergrondafbeelding uit Figma vervangen door blanco pixel op egaal witte achtergrond

--- src/screens/ScanPreviewScreen.tsx
+++ src/screens/ScanPreviewScreen.tsx
@@ -13,8 +13,6 @@ import {
 } from 'react-native';
 import DocumentScanner, { ResponseType, ScanDocumentResponseStatus } from 'react-native-document-scanner-plugin';
 
-// @ts-ignore
-import DefaultImagePreviewBackground from '../../assets/default-image-preview-background.png';
 import SvgArrowLeftIcon from '../../assets/icons/arrow-round-left.svg';
 import SvgEllipsisIcon from '../../assets/icons/ellipsis-icon.svg';
 import SvgPlusIcon from '../../assets/icons/plus-icon.svg';
@@ -70,29 +68,51 @@ export const ScanPreviewScreen: React.FC<Props> = ({ navigation }) => {
         responseType: ResponseType.ImageFilePath,
       });
 
+      setHasPendingImages(true);
+      const rotatedImagePaths: string[] = [];
+
       // if the status is not Success, the user cancelled
       if (result.status === ScanDocumentResponseStatus.Success) {
-        setHasPendingImages(true);
         const imagePaths = result.scannedImages || [];
-        const rotatedImagePaths: string[] = [];
 
         for (let idx = 0; idx < imagePaths.length; idx++) {
           rotatedImagePaths.push(await rotateImageIfNeeded(imagePaths[idx]));
         }
+      }
 
+      if (images.length === 0 && rotatedImagePaths.length === 0) {
+        navigation.navigate('Dashboard');
+        // note: do not reset hasPendingImages here or the useEffect() hook will re-open the camera while navigating
+      } else {
         addImages(rotatedImagePaths);
         setHasPendingImages(false);
+
+        if (selectedImageIndex === undefined) {
+          selectImage(0);
+        }
       }
     } catch (reason) {
       captureError(reason, 'An error occurred while scanning documents');
       setHasPendingImages(false);
     }
-  }, [addImages, setHasPendingImages]);
+  }, [addImages, images, navigation, selectedImageIndex, setHasPendingImages, selectImage]);
+
+  useEffect(() => {
+    if (!hasPendingImages && images.length === 0) {
+      openCamera()
+        .then()
+        .catch((reason) => {
+          captureError(reason, 'An error occurred while scanning documents');
+        });
+    }
+  }, [images, hasPendingImages, openCamera]);
 
   const backgroundImage = useMemo(
     () => {
       if (images.length === 0 || selectedImageIndex === undefined) {
-        return DefaultImagePreviewBackground;
+        // <ImageBackground /> requires an image, set a 1x1 pixel transparent PNG
+        // eslint-disable-next-line max-len
+        return { uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=' };
       }
 
       if (selectedImageIndex >= images.length) {
