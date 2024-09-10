80957281ca3d4012286837675983c0f64687333a
Yasen Dinkov <42535596+b10-ydinkov@users.noreply.github.com>
2024-09-10 11:44:25 +0200

    Changed Deleted user property

--- src/api/UserApi.ts
+++ src/api/UserApi.ts
@@ -6,7 +6,7 @@ export class UserApi extends ApiServiceRequests {
    * Get all users including deleted once.
    */
   public async getAllUsers(): Promise<getAllUsersResponse> {
-    const { data } = await this.getAxios().get<getAllUsersResponse>('/User?includeDeleted=true');
+    const { data } = await this.getAxios().get<getAllUsersResponse>('/User?includeDeleted=false');
 
     return data;
   }
