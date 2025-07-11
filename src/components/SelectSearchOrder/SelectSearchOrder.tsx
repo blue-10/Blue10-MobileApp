90375e5f8f793ba13b270d86e35d1312e4089f18
a.moslemi.68@gmail.com <a.moslemi.68@gmail.com>
2025-07-11 11:27:36 +0200

    fix translate bug

--- src/components/SelectSearchOrder/SelectSearchOrder.tsx
+++ src/components/SelectSearchOrder/SelectSearchOrder.tsx
@@ -7,18 +7,20 @@ import { t } from 'i18next';
 
 type Props = Omit<SelectProps, 'items'>;
 
-const data = [
-  { id: 'D_DocumentDate', name: t('search_filter.order_items.date_desc') },
-  { id: 'A_DocumentDate', name: t('search_filter.order_items.date_asc') },
-  { id: 'D_DocumentNumber', name: t('search_filter.order_items.invoiceNR_desc') },
-  { id: 'A_DocumentNumber', name: t('search_filter.order_items.invoiceNR_asc') },
-  { id: 'D_DueDate', name: t('search_filter.order_items.dueDate_desc') },
-  { id: 'A_DueDate', name: t('search_filter.order_items.dueDate_asc') },
-  { id: 'D_RelationName', name: t('search_filter.order_items.vendor_desc') },
-  { id: 'A_RelationName', name: t('search_filter.order_items.vendor_asc') },
-];
-
 export const SelectSearchOrder: React.FC<Props> = ({ ...selectProps }) => {
+  const data = useMemo(
+    () => [
+      { id: 'D_DocumentDate', name: t('search_filter.order_items.date_desc') },
+      { id: 'A_DocumentDate', name: t('search_filter.order_items.date_asc') },
+      { id: 'D_DocumentNumber', name: t('search_filter.order_items.invoiceNR_desc') },
+      { id: 'A_DocumentNumber', name: t('search_filter.order_items.invoiceNR_asc') },
+      { id: 'D_DueDate', name: t('search_filter.order_items.dueDate_desc') },
+      { id: 'A_DueDate', name: t('search_filter.order_items.dueDate_asc') },
+      { id: 'D_RelationName', name: t('search_filter.order_items.vendor_desc') },
+      { id: 'A_RelationName', name: t('search_filter.order_items.vendor_asc') },
+    ],
+    [t],
+  );
   const items = useMemo(() => {
     return data.map((item) => ({
       title: item.name,
