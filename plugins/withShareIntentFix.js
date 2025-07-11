c80dd80ac90027d219e8695c0e1c83aa140eb136
a.moslemi.68@gmail.com <a.moslemi.68@gmail.com>
2025-07-11 14:37:03 +0200

    Update plugin to prevent sharing PDF files

--- plugins/withShareIntentFix.js
+++ plugins/withShareIntentFix.js
@@ -1,10 +1,41 @@
+const { withAndroidManifest } = require('@expo/config-plugins');
 const { withMainActivity } = require('@expo/config-plugins/build/plugins/android-plugins');
 
-const withShareIntentFix = (config) => {
-  return withMainActivity(config, (config) => {
-    if (config.modResults.language === 'java') return config;
+const withShareIntentImageOnly = (config) => {
+  config = withAndroidManifest(config, (modConfig) => {
+    const mainActivity = modConfig.modResults.manifest.application[0].activity.find(
+      (a) => a.$['android:name'] === '.MainActivity',
+    );
 
-    let src = config.modResults.contents;
+    mainActivity['intent-filter'] = (mainActivity['intent-filter'] || []).filter(
+      (f) =>
+        !f.action?.some(
+          (a) =>
+            a.$['android:name'] === 'android.intent.action.SEND' ||
+            a.$['android:name'] === 'android.intent.action.SEND_MULTIPLE',
+        ),
+    );
+
+    mainActivity['intent-filter'].push({
+      action: [{ $: { 'android:name': 'android.intent.action.SEND' } }],
+      category: [{ $: { 'android:name': 'android.intent.category.DEFAULT' } }],
+      data: [{ $: { 'android:mimeType': 'image/*' } }],
+    });
+    mainActivity['intent-filter'].push({
+      action: [{ $: { 'android:name': 'android.intent.action.SEND_MULTIPLE' } }],
+      category: [{ $: { 'android:name': 'android.intent.category.DEFAULT' } }],
+      data: [{ $: { 'android:mimeType': 'image/*' } }],
+    });
+
+    mainActivity.$['android:exported'] = 'true';
+
+    return modConfig;
+  });
+
+  config = withMainActivity(config, (modConfig) => {
+    if (modConfig.modResults.language === 'java') return modConfig;
+
+    let src = modConfig.modResults.contents;
 
     if (!src.includes('override fun onNewIntent')) {
       src = src.replace(
@@ -18,11 +49,13 @@ const withShareIntentFix = (config) => {
 `,
       );
 
-      config.modResults.contents = src;
+      modConfig.modResults.contents = src;
     }
 
-    return config;
+    return modConfig;
   });
+
+  return config;
 };
 
-module.exports = withShareIntentFix;
+module.exports = withShareIntentImageOnly;
