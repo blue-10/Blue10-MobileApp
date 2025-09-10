4b39256ac682d88c3453113369f43f1ad9e3aca5
AtefeMoslemiBlue10 <am@blue10.com>
2025-09-10 13:46:19 +0200

    implement login in external browser

--- src/components/LoginSite/LoginSite.tsx
+++ src/components/LoginSite/LoginSite.tsx
@@ -1,8 +1,7 @@
 import Constants from 'expo-constants';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { StyleSheet } from 'react-native';
-import WebView from 'react-native-webview';
+import { Button, Linking, SafeAreaView, StyleSheet } from 'react-native';
 
 import { authConstants, lngConvert } from '../../constants';
 import { makeCookies, parseCookies } from '../../utils/cookiesUtils';
@@ -58,11 +57,10 @@ const urlToApiUrl = (url: string) => {
 const isTooManyRedirectError = (code: number) => code === -9 || code === -1007;
 
 const LoginSite: React.FC<LoginSiteProps> = ({ mode, refreshToken, onRefreshToken }) => {
-  const webViewRef = useRef<WebView>(null);
-  const [webViewError, setWebViewError] = useState<WebViewError | undefined>();
   const [uri, setUri] = useState(mode === 'environment' ? authConstants.switchEnvironment : authConstants.loginPage);
   const { i18n } = useTranslation();
   const locale = lngConvert[i18n.language];
+  const { t } = useTranslation();
 
   const defaultHeaders = {
     'Accept-Language': `${locale},${i18n.language};q=0.5`,
@@ -86,66 +84,20 @@ const LoginSite: React.FC<LoginSiteProps> = ({ mode, refreshToken, onRefreshToke
   };
   const headers = mode === 'environment' ? envHeaders : defaultHeaders;
   return (
-    <>
-      <WebView
-        ref={webViewRef}
-        hideKeyboardAccessoryView
-        incognito={true}
-        injectedJavaScript={LOGIN_COOKIE_READER}
-        renderError={() => <></>}
-        renderLoading={() => <LoginSiteLoader />}
-        setBuiltInZoomControls={false}
-        source={{
-          headers,
-          uri,
-        }}
-        startInLoadingState={true}
-        style={styles.webView}
-        onError={(event) => {
-          const { nativeEvent } = event;
-          setWebViewError({
-            errorCode: nativeEvent.code,
-            errorDescription: nativeEvent.description,
-            errorName: nativeEvent.domain,
-          });
-        }}
-        onMessage={(data) => {
-          const { url, data: cookieData } = data.nativeEvent;
-          if (url && !isUrlFromLogin(url)) {
-            const { refresh_token } = parseCookies(cookieData);
-            if (refresh_token) {
-              onRefreshToken(refresh_token, urlToApiUrl(url));
-            }
-          }
-        }}
-        onShouldStartLoadWithRequest={() => true}
-      />
-      {webViewError && (
-        <LoginSiteError
-          errorCode={webViewError.errorCode}
-          errorDescription={webViewError.errorDescription}
-          errorName={webViewError.errorName}
-          onRetry={() => {
-            const isTooManyRedirects = isTooManyRedirectError(webViewError.errorCode);
-            if (isTooManyRedirects) {
-              // there is no function to navigate to page. so we just change the uri to about:blank and back.
-              setUri('about:blank');
-              requestAnimationFrame(() => setUri(authConstants.loginPage));
-            } else {
-              webViewRef.current?.reload();
-            }
-            setWebViewError(undefined);
-          }}
-        />
-      )}
-    </>
+    <SafeAreaView style={styles.container}>
+      <Button onPress={() => Linking.openURL('https://login.blue10development.com/?mobile=true')} title={t("Login")} />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  webView: {
+  container: {
     flex: 1,
+    justifyContent: 'center',
+    width: '50%',
+    margin: 'auto',
   },
 });
+
 
 export default LoginSite;
